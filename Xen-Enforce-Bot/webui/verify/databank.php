5db85bd9266620fcc71ff93378d69c8f7d677b37
faelan <faelan.galanti@gmail.com>
2020-11-19 19:15:07 -0500

    Add mysql ssl configuration.

--- Xen-Enforce-Bot/webui/verify/databank.php
+++ Xen-Enforce-Bot/webui/verify/databank.php
@@ -13,29 +13,27 @@
 	$dbdatabase = "{{ xen_mysql_database }}";
 	$dbtable = "{{ xen_mysql_verify_table }}";
 
-
-
-
 	if (!function_exists("resultToArray")) {
 		function resultToArray($result) {
-		    $rows = array();
-		    while($row = $result->fetch_assoc()) {
-		        $rows[] = $row;
-		    }
-		    return $rows;
+			$rows = array();
+			while($row = $result->fetch_assoc()) {
+				$rows[] = $row;
+			}
+			return $rows;
 		}
 	}
 
+    $DB_OBJ = mysqli_init();
+    $DB_OBJ->ssl_set(
+		'{{ xen_mysql_client_key }}',
+		'{{ xen_mysql_client_cert }}',
+		'{{ xen_mysql_ca }}',
+		NULL,
+		NULL
+	);
+	$DB_OBJ->real_connect($host, $user, $password, $dbdatabase, NULL, NULL,
+                          MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT)  // TODO IF insecure ... ELSE ...
+                          or die('Failed to connect: ' . $DB_OBJ->error);
+	$DB_OBJ->set_charset('utf8');
 
-
-
-
-
-	$DB_OBJ = new mysqli($host, $user, $password, $dbdatabase)
-		or die('Failed to connect: ' . mysqli_error());
-		mysqli_set_charset($DB_OBJ, "utf8");
-	if ($DB_OBJ->connect_errno) {
-			echo "Database connection failed";
-			exit();
-	}
 ?>
